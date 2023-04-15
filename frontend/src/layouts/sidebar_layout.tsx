9dbf86c61a17e63a1dcd61e5e7ee40891c889643
Thomas Guillouët <thomas.guillouet@outlook.com>
2023-04-15 09:42:30 +0200

    Logging out

--- frontend/src/layouts/sidebar_layout.tsx
+++ frontend/src/layouts/sidebar_layout.tsx
@@ -1,8 +1,10 @@
 import styles from "@/styles/layout/sidebar.module.css";
 import Sidebar from "@/components/sidebar/Sidebar"
-import { ReactElement, useEffect, useState } from "react"
+import { ReactElement, useCallback, useEffect, useState } from "react"
 import Breadcrumb from "@/components/breadcrumb/Breadcrumb";
 import { useRouter } from "next/router";
+import { Button, OverlayTrigger, Popover } from "react-bootstrap";
+import Cookies from "js-cookie";
 
 type SidebarLayoutProps = {
   title: string,
@@ -21,12 +23,32 @@ export function SidebarLayout({ title, children }: SidebarLayoutProps) {
     setPath(pathArray);
   }, [router]);
 
+  const onClickLogout = useCallback(() => {
+    Cookies.remove("token");
+    router.replace("/");
+  }, [router]);
+
   return (
     <main className={styles.sidebar_layout}>
       <Sidebar />
 
       <section>
-        <h1 className={styles.sidebar__title}>{title}</h1>
+        <div className={styles.sidebar__content_header}>
+          <h1 className={styles.sidebar__title}>{title}</h1>
+
+          <OverlayTrigger trigger="click" placement="bottom" overlay={(
+            <Popover>
+              <Popover.Body>
+                <Button className={styles.btn_link} variant="secondary" onClick={onClickLogout}>
+                  Se déconnecter
+                </Button>
+              </Popover.Body>
+            </Popover>
+          )}>
+            <div className={styles.user_icon}>
+            </div>
+          </OverlayTrigger>
+        </div>
         <Breadcrumb path={path} />
 
         <div className={styles.sidebar__content}>
